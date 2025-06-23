33b208696cda2226aa49721689981d4babaa3e4f
Harikarthick <147905893+Harikarthick28@users.noreply.github.com>
2025-06-23 22:52:43 +0530

    Update pow.java

--- 23.06.2025/pow.java
+++ 23.06.2025/pow.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); // number of test cases
+        int t = sc.nextInt(); 
 
         for (int i = 0; i < t; i++) {
-            int a = sc.nextInt(); // starting value
-            int b = sc.nextInt(); // increment
-            int n = sc.nextInt(); // number of terms
+            int a = sc.nextInt(); 
+            int b = sc.nextInt(); 
+            int n = sc.nextInt(); 
 
             int result = a;
             for (int j = 0; j < n; j++) {
                 result += (int) Math.pow(2, j) * b;
                 System.out.print(result + " ");
             }
-            System.out.println(); // move to next line for next test case
+            System.out.println(); 
         }
     }
 }
